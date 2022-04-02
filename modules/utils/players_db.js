05fac094e15b4575b6ec08324d686745db4c4793
Asurin219 <824907403@qq.com>
2022-04-02 23:54:19 +0800

    修正SQL语句、逻辑

--- modules/utils/players_db.js
+++ modules/utils/players_db.js
@@ -55,7 +55,7 @@ init_sql += 'CREATE TABLE IF NOT EXISTS SERVERS (ID INTEGER PRIMARY KEY AUTOINCR
 init_sql += 'CREATE TABLE IF NOT EXISTS ALLOWLIST_DATA (ID INTEGER PRIMARY KEY AUTOINCREMENT, Server_ID INTEGER REFERENCES SERVERS (ID) ON DELETE CASCADE, Player_ID INTEGER REFERENCES PLAYERS (ID) ON DELETE CASCADE);'
 
 for (let i in servers) {    //服务器表写入数据
-    init_sql += 'INSERT INTO SERVERS(Server_Name) SELECT\'' + i + '\' WHERE NOT EXISTS(SELECT * FROM SERVERS WHERE Server_Name = \''+i+'\');'
+    init_sql += 'INSERT INTO SERVERS(Server_Name) SELECT\'' + i + '\' WHERE NOT EXISTS(SELECT * FROM SERVERS WHERE Server_Name = \'' + i + '\');'
 }
 
 db_exec(init_sql)
@@ -97,50 +97,43 @@ LB.PlayersDB.bindXboxID = async (groupid, tips, qq, xboxid) => {
  * @return {Promise<void>}
  */
 LB.PlayersDB.addAllowlist = async (groupid, tips, bind_servers, qq) => {
-    let select_sql = `SELECT P.QQ_ID, P.Xbox_ID, S.Server_Name FROM PLAYERS P LEFT JOIN ALLOWLIST_DATA A LEFT JOIN SERVERS S ON A.Player_ID = P.ID AND A.Server_ID = S.ID WHERE P.QQ_ID = '${qq}'`
-
-    await db_all(select_sql, (res) => {
+    let xboxid
+    let sql0 = `SELECT Xbox_ID FROM PLAYERS WHERE QQ_ID='${qq}'`
+    await db_all(sql0, (res) => {
         if (res[0] == undefined) {
             LB.Groups.sendMsg(groupid, allowlistEvent(tips[0], qq))   //联查无记录，未绑定
             return
         }
+        xboxid = res[0].Xbox_ID
+    })
 
-        let binded_servers = [], xboxid
-        for (let i in res) {
-            if (res[i].Server_Name != null) {   //联查有记录，收集结果
-                binded_servers.push(res[i].Server_Name)
-            }
-            if (binded_servers.length == bind_servers.length && binded_servers.every((v, n) => {
-                return v === binded_servers[n]
-            })) { //联查结果中的服务器 == 需要添加白名单的服务器
-                LB.Groups.sendMsg(groupid, allowlistEvent(tips[1], qq, res[0].Xbox_ID))     //已经添加过白名单了
-                return
-            }
-            xboxid = res[i].Xbox_ID
-        }
+    let sql1 = `SELECT P.QQ_ID, P.Xbox_ID, S.Server_Name FROM PLAYERS P,ALLOWLIST_DATA A,SERVERS S WHERE A.Player_ID = P.ID AND A.Server_ID = S.ID AND P.QQ_ID='${qq}'`
+    await db_all(sql1, (res) => {
+        if (res[0] == undefined) {
+            let update_sql = ''
 
-        let update_sql = ''
-
-        bind_servers.forEach(i => {
-            try {
-                if (!LB.WS.servers[i].isOK()) {   //连接异常（连接中断、出现错误等）
-                    LB.Groups.sendMsg(groupid, allowlistEvent('server_connect_error', bind_servers, qq))
+            bind_servers.forEach(i => {
+                try {
+                    if (!LB.WS.servers[i].isOK()) {   //连接异常（连接中断、出现错误等）
+                        LB.Groups.sendMsg(groupid, allowlistEvent('server_connect_error', bind_servers, qq))
+                        return
+                    }
+                } catch (err) { //其他异常（一般只出现在未启用的时候）
+                    log.error(err)
                     return
                 }
-            } catch (err) { //其他异常（一般只出现在未启用的时候）
-                log.error(err)
-                return
-            }
-            LB.WS.servers[i].runCMD(`allowlist add ${xboxid}`)
-            update_sql += `INSERT INTO ALLOWLIST_DATA(Server_ID,Player_ID) SELECT S.ID,P.ID FROM SERVERS S INNER JOIN PLAYERS P ON S.Server_Name='${i}' AND P.QQ_ID = '${qq}';`
-        })
+                LB.WS.servers[i].runCMD(`allowlist add ${xboxid}`)
+                update_sql += `INSERT INTO ALLOWLIST_DATA(Server_ID,Player_ID) SELECT S.ID,P.ID FROM SERVERS S INNER JOIN PLAYERS P ON S.Server_Name='${i}' AND P.QQ_ID = '${qq}';`
+            })
 
-        db_exec(update_sql)
-        LB.Groups.src = groupid
-        LB.Groups.sendMsg(groupid, allowlistEvent('adding_to_allowlist', qq, res[0].Xbox_ID))  //白名单添加成功
+            db_exec(update_sql)
+            LB.Groups.src = groupid
+            LB.Groups.sendMsg(groupid, allowlistEvent('adding_to_allowlist', qq, xboxid))  //白名单添加成功
+        } else {
+            LB.Groups.sendMsg(groupid, allowlistEvent(tips[1], qq, xboxid))     //已经添加过白名单了
+        }
     })
 }
-
 
 /**
  * 查询绑定信息
@@ -150,29 +143,34 @@ LB.PlayersDB.addAllowlist = async (groupid, tips, bind_servers, qq) => {
  * @return {Promise<void>}
  */
 LB.PlayersDB.getBindInfo = async (groupid, tips, qq) => {
-    let select_sql = `SELECT P.QQ_ID, P.Xbox_ID, S.Server_Name FROM PLAYERS P LEFT JOIN ALLOWLIST_DATA A LEFT JOIN SERVERS S ON A.Player_ID = P.ID AND A.Server_ID = S.ID WHERE P.QQ_ID = '${qq}'`
+    let xboxid, binded = []
 
-    await db_all(select_sql, (res) => {
-        log.debug(`\n绑定信息：\n${JSON.stringify(res,null,'\t')}`)
+    let sql0 = `SELECT QQ_ID,Xbox_ID FROM PLAYERS WHERE QQ_ID='${qq}'`
+    await db_all(sql0, (res) => {
+        log.debug(`\n绑定信息：\n${JSON.stringify(res, null, '\t')}`)
         if (res[0] == undefined) {
-            LB.Groups.sendMsg(groupid, allowlistEvent(tips, qq))   //主表（玩家表）无记录，未绑定
+            LB.Groups.sendMsg(groupid, allowlistEvent(tips, qq))   //联查无记录，未绑定
             return
         }
+        xboxid = res[0].Xbox_ID
+    })
 
-        let binded = [], xboxid
-        for (let i in res) {
-            xboxid = res[i].Xbox_ID
-            if (res[i].Server_Name == null) {
-                binded.push("没有记录")    //主表（玩家表）有记录，但白名单表无记录，已绑定但未添加白名单
-                break
-            } else {
+    let sql1 = `SELECT P.QQ_ID, P.Xbox_ID, S.Server_Name FROM PLAYERS P,ALLOWLIST_DATA A,SERVERS S WHERE A.Player_ID = P.ID AND A.Server_ID = S.ID AND P.QQ_ID='${qq}'`
+    await db_all(sql1, (res) => {
+        log.debug(`\n白名单信息：\n${JSON.stringify(res, null, '\t')}`)
+        if (res[0] == undefined) {
+            if(xboxid == undefined) return
+            binded.push("没有记录")
+        } else {
+            for (let i in res) {
                 binded.push(res[i].Server_Name)
             }
         }
+
+
         let status = binded.toString(),
             perm = ['普通成员', '管理员'],
             n = Number(groups[groupid].admin.some((val) => val === qq))
-
 
         LB.Groups.sendMsg(groupid, allowlistEvent('get_bind_info', qq, xboxid, perm[n], status))
     })
