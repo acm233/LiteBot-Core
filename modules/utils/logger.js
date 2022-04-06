7416595b45ca8d4f2a9809cb6c0ba3a4b4fb4ade
Asurin219 <824907403@qq.com>
2022-04-06 21:45:41 +0800

    修复debug开关不生效的问题

--- modules/utils/logger.js
+++ modules/utils/logger.js
@@ -21,7 +21,7 @@ LB.log = class {
     }
 
     debug() {
-        if(!LB.conf.isExists('./config/global_config.yml') || !LB.conf.readFrom('./config/global_config.yml')) return
+        if(!LB.conf.isExists('./config/global_config.yml') || !LB.conf.readFrom('./config/global_config.yml').debug_mode) return
         log_output('debug', Object.values(arguments).join(this.Text), this.module)
     }
 
