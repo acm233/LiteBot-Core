38a624a671dfc544b9cce9fbad4e29ef6ef9ca17
Asurin219 <824907403@qq.com>
2022-04-06 17:57:32 +0800

    fixed

--- plugins/demo.js
+++ plugins/demo.js
@@ -2,12 +2,12 @@ const log = new LB.log('Demo Plugins');
 
 log.info("我是Demo");
 
-LB.OICQ.onEvent('notice.group.increase',e=>{
+LB.OICQ.onEvent('notice.group.increase',(e,groupid)=>{
     log.debug('新人来辣！')
     console.log(e)
 })
 
-LB.OICQ.onEvent('notice.group.decrease',e=>{
+LB.OICQ.onEvent('notice.group.decrease',(e,groupid)=>{
     log.debug('牙败，有人退群！')
     console.log(e)
 })
